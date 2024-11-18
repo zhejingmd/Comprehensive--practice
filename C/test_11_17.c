2a0d1f2b7f5febeefd40f52b9e7ff118bc1b39bf
折镜 <2565874117@qq.com>
2024-11-18 18:46:43 +0800

    11.18 commit

---
+++ C/test_11_17.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// 6.3 结构数组
+
+// #define NKEYS (sizeof keytab / sizeof(keytab[0]))
+// struct key
+// {
+//     char *word;
+//     int count;
+// } keytab[];
+
+// 等价于
+// struct key {...}
+// struct key keytab[];
+
+
---
+++ C/test_11_18.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+// 6.4 指向结构的指针
+
+// 6.5 自引用结构
+
+// 6.6 表查找
+
+// 6.7 类型定义（typedef）
+
+// typedef int Length; // 将Length定义为与 int 具有相同意义的名字
+
+// 第七章 输入与输出
+
+// 7.1 标准输入/输出
+
+// #include <ctype.h>
+// 将输入转换为小写
+// int main()
+// {
+//     int c;
+//     while ((c = getchar()) != EOF)
+//         putchar(tolower(c));     // tolower 在<ctype.h>中定义，把大写字母转换为小写，原样返回
+//     return 0;
+// }
+
+
+
+
